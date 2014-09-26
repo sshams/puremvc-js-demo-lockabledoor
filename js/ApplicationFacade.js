c1811c0db2ef7a89b9018061d36099fe5ed4b4f9
Saad Shams <saad.shams@casa.ag>
2014-09-26 18:50:17 -0400

    init

--- js/ApplicationFacade.js
+++ js/ApplicationFacade.js
@@ -1,3 +1,8 @@
+/*
+ PureMVC MultiCore Demo for JS - Lockable Door
+ Copyright(c) 2014 Saad Shams <saad.shams@puremvc.org>
+ Your reuse is governed by the Creative Commons Attribution 3.0 License
+*/
 puremvc.define(
 {
     name: 'ApplicationFacade',
